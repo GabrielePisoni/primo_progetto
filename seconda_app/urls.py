18e2cf268b5e08d5b50e3e090d2cc8686086daea
GabrielePisoni <gabriele.pisoni@alessandrinimainardi.edu.it>
2022-10-10 10:49:08 +0200

    Passo 9

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import es_if, es_if_elif
+from .views import es_if, es_if_elif, es_for
 app_name = "seconda_app"
 urlpatterns = [
     path('es_if', es_if, name='es_if'),
     path('es_if_elif', es_if_elif, name='es_if_elif'),
+    path('es_for', es_for, name='es_for'),
 ]
