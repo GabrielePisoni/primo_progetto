9994496de3dc443657919cc5c8424e80c094d75f
GabrielePisoni <gabriele.pisoni@alessandrinimainardi.edu.it>
2022-10-17 09:19:47 +0200

    bootstrap
    bootstrap

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -17,11 +17,11 @@ from operator import index
 from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
-from .views import index_generale
+from .views import index_generale, base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('prima_app/', include('prima_app.urls', namespace='prima_app')),
     path('seconda_app/', include('seconda_app.urls', namespace='seconda_app')),
-    path('', index_generale, name="index_generale"),
+    path('', base, name="base"),
 ]
