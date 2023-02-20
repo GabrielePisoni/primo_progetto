957b24c681675ecad39f8dfbbbfc4a72304d29d0
GabrielePisoni <gabriele.pisoni@alessandrinimainardi.edu.it>
2023-02-20 15:46:00 +0100

    art

--- news/urls.py
+++ news/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import home, articoloDetailViewCB, articoloListView, GiornalistaDetailViewCB, GiornalistaListView #articoloDetailView
+from .views import home, ArticoloDetailViewCB,ArticoloListView,GiornalistaDetailViewCB,GiornalistaListView 
 
-app_name = 'news'
+app_name="news"
 
 urlpatterns = [
-    path('', home, name="articoli"),
-    # path('articoli/<int:pk>', articoloDetailView, name="articolo_detail")
-    path('articoli/<int:pk>', articoloDetailViewCB.as_view(), name="articolo_detail"),
-    path("lista_articoli/", articoloListView.as_view(), name="lista_articoli"),
-    path("giornalisti/<int:pk>", GiornalistaDetailViewCB.as_view(), name="giornalista_detail"),
-    path("lista_giornalisti/", GiornalistaListView.as_view(), name="lista_giornalisti"),
-
+    path('', home, name="homeview"),
+    path('articoli/<int:pk>',ArticoloDetailViewCB.as_view(),name="articolo_detail"),
+    path('lista_articoli/',ArticoloListView.as_view(),name="lista_articoli"),
+    path('giornalisti/<int:pk>',GiornalistaDetailViewCB.as_view(),name="giornalista_detail"),
+    path('lista_giornalisti/',GiornalistaListView.as_view(),name="lista_giornalisti"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,71 +1,51 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Articolo, Giornalista
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Articolo,Giornalista
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.http import JsonResponse
+from django.core.serializers import serialize
+import json
 
 # Create your views here.
 
-
-# def home(request):
-#     a = ""   tramite il += concateniamo i vari articoli e giornalisti
-#     g = ""   e li stampiamo
-#     for art in Articolo.objects.all():
-#         a += (art.titolo + "<br>")
-
-#     for gio in Giornalista.objects.all():
-#         g += (gio.nome + "<br>")
-#     response = "articoli:<br>" + a + "<br>Giornalisti:<br>" + g
-
-#     return HttpResponse("<h1>" + response + "</h1>")
-
-# def home(request):
-#     a = []  possiamo creare una lista degli articoli e giornalisti
-#     g = []  all'interno di un vettore
-#     for art in Articolo.objects.all():
-#         a.append(art.titolo)
-
-#     for gio in Giornalista.objects.all():
-#         g.append(gio.nome)
-
-#     response = str(a) + "<br>" + str(g)
-#     print(response)
-
-#     return HttpResponse("<h1>" + response + "</h1>")
-
-def home (request):
-    articoli = Articolo.objects.all()
-    giornalisti = Giornalista.objects.all()
-    context = {"articoli": articoli, "giornaisti": giornalisti}
+def home(request):
+    articoli=Articolo.objects.all()
+    giornalisti=Giornalista.objects.all()
+    context={'articoli':articoli,'giornalisti':giornalisti}
     print(context)
-    return render(request, "articoli.html", context)
+    return render(request,"homepage2.html",context)
 
-def articoloDetailView(request, pk):
-    articolo = get_object_or_404(Articolo, pk=pk)
-    context = {"articolo": articolo}
-    return render(request, "articolo_detail.html", context)
+def articoloDetailView(request,pk):
+    # articolo=Articolo.objects.get(pk=pk)
+    articolo=get_object_or_404(Articolo,pk=pk)
+    context={'articolo':articolo}
+    return render(request,"articolo_detail.html",context)
 
-class articoloDetailViewCB(DetailView):
-    model = Articolo
-    template_name = "articolo_detail.html"
 
-class articoloListView(ListView):
-    model = Articolo
-    template_name = "lista_articoli.html"
+class ArticoloDetailViewCB(DetailView):
+    model=Articolo
+    template_name="articolo_detail.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["articoli"] = Articolo.objects.all()
+class ArticoloListView(ListView):
+    model=Articolo
+    template_name="lista_articoli.html"
+
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['articoli']=Articolo.objects.all()
         return context
 
 class GiornalistaDetailViewCB(DetailView):
-    model = Articolo
-    template_name = "giornalista_detail.html"
+    model=Giornalista
+    template_name="giornalista_detail.html"
 
 class GiornalistaListView(ListView):
-    model = Articolo
-    template_name = "lista_giornalisti.html"
+    model=Giornalista
+    template_name="lista_giornalisti.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["articoli"] = Articolo.objects.all()
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['giornalisti']=Giornalista.objects.all()
         return context
